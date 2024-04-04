1a52f6199ac897183055e8a0c091944d11e2f5a0
Aadhavan <152236707+Aadhavan2906@users.noreply.github.com>
2024-04-04 14:47:09 +0530

    Add files via upload

---
+++ Trees_in_Java-main/PostOrder.java
@@ -0,0 +1,93 @@
+//write your program here
+import java.util.*;
+class Node
+  {
+    int data;
+    Node left;
+    Node right;
+    Node(int data)
+    {
+      this.data=data;
+      this.right=null;
+      this.left=null;
+    }
+  }
+class BST
+  {
+    Node root;
+    BST()
+    {
+      this.root=null;
+    }
+    void insert(int data)
+    {
+      Node newnode=new Node(data);
+      if(root==null)
+        root=newnode;
+      else
+      {
+        Node temp=root;
+        while(true)
+          {
+            if(temp.data>newnode.data)
+            {
+              if(temp.left==null)
+              {
+                temp.left=newnode;
+                break;
+              }
+              else
+              {
+                temp=temp.left;
+              }
+            }
+            else
+            {
+              if(temp.data<newnode.data)
+              {
+                if(temp.right==null)
+                {
+                  temp.right=newnode;
+                  break;
+                }
+                else
+                {
+                  temp=temp.right;
+                }
+              }
+            }
+          }
+      }
+        
+    }
+    void display()
+    {
+displayrec(root);
+    }
+    void displayrec(Node root)
+    {
+      if(root==null)
+        return;
+      displayrec(root.left);
+      displayrec(root.right);
+      System.out.print(root.data+" ");
+    }
+  }
+class Main
+  {
+     public static void main(String args[])
+    {
+      Scanner d=new Scanner(System.in);
+      BST b=new BST();
+      int n;
+      while(true)
+        {
+          n=d.nextInt();
+          if(n>0)
+            b.insert(n);
+          else
+            break;
+        }
+      b.display();
+    }
+  }
